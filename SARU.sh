8eb83311996c995a96867bdbd2edd9f54e06fcc5
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-21 11:59:24 +0100

    Update SARU.sh

--- SARU.sh
+++ SARU.sh
@@ -8,25 +8,20 @@ useradd -m -g users -G wheel,storage,power -s /bin/bash $username
 echo enter password for the user $username
 passwd $username 
 
-pacman -S  --noconfirm --needed git base-devel
-
 pacman -S  --noconfirm xfce4 xfce4-goodies terminator lightdm lightdm-gtk-greeter pavucontrol fish doas
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+systemctl enable lightdm
 
 echo permit :wheel > /etc/doas.conf
 echo permit nopass keepenv root >> /etc/doas.conf
 
+doas -u $username git clone https://aur.archlinux.org/yay.git  && cd yay && makepkg -si
+
 chsh -s /bin/fish $username
+echo set fish_greeting >> /home/$username/.config/fish/config.fish
 
 doas -u $username yay -S --noconfirm paper-icon-theme Equilux-theme
 
 doas -u $username xfconf-query -c xsettings -p /Net/ThemeName -s "Equilux"
 doas -u $username xfconf-query -c xsettings -p /Net/IconThemeName -S "Paper"
 
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
-omf install agnoster
-echo set fish_greeting >> /home/$username/.config/fish/config.fish
 
