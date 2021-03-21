3bb4259f4fc53b0edc5689e4afa96060446ccd4e
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-21 17:38:06 +0100

    Update SARU.sh

--- SARU.sh
+++ SARU.sh
@@ -42,4 +42,8 @@ mkdir --parents /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml
 cp xfce4-desktop.xml /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml
 cp xsettings.xml /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml
 cp xfce4-panel.xml /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml
+cp xfce4-clipman-actions.xml /home/$username/.config/xfce4/panel
+cp helpers.rc /home/$username/.config/xfce4/
+cd default
+cp * /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml
 reboot
