28463dd9084479d0cb59f2a019ca8e5f945bb67e
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-22 19:31:11 +0100

    Update SARU.sh

--- SARU.sh
+++ SARU.sh
@@ -47,7 +47,7 @@ cp lightdm-gtk-greeter.conf /etc/lightdm
 echo 'section "InputClass"' > /etc/X11/xorg.conf.d/00-keyboard.conf
 echo '    Identifier "system-keyboard"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
 echo '    MatchIsKeyboard "on"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
-echo '    Option "XkbLayout" \"$LANG\"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+echo '    Option "XkbLayout" "\""${LANG}"\""' >> /etc/X11/xorg.conf.d/00-keyboard.conf
 echo 'EndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf
 
 reboot
