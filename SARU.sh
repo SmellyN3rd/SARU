8f6e9c7ab59234c6f95fb88a338d0ccb1547042f
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-23 17:49:45 +0100

    Update SARU.sh

--- SARU.sh
+++ SARU.sh
@@ -37,7 +37,7 @@ echo -ne installing other programs... && pacman -S --noconfirm feh mpv neovim zs
 
 echo -ne configuring the shell... && chsh -s /bin/zsh $username &> /dev/null
 cd /home/$username
-"$(doas -u $username curl -L http://install.ohmyz.sh | doas -u $username sh)" &> /dev/null
+"$(doas -u $username curl -sL http://install.ohmyz.sh | doas -u $username sh)" &> /dev/null
 "$(doas -u $username git clone https://github.com/zsh-users/zsh-autosuggestions /home/$username/.zsh/zsh-autosuggestions)" &> /dev/null && echo done
 
 echo -ne installing the system theme... && doas -u $username yay -S --noconfirm equilux-theme &> /dev/null && pacman --noconfirm -S papirus-icon-theme &> /dev/null && echo done
