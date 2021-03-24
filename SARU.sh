a65c04b5a524882ccedafde9eae2f76a7b516d07
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-24 22:05:29 +0100

    Update SARU.sh

--- SARU.sh
+++ SARU.sh
@@ -64,7 +64,7 @@ echo done
 
 echo -ne configuring the shell...
 cd /home/$username
-doas -u $username curl -sL install.ohmyz.sh | sh
+sudo -u $username curl -sL install.ohmyz.sh | sh
 sleep 10
 pacman --noconfirm -S zsh-syntax-highlighting &> /dev/null
 doas -u $username git clone https://github.com/zsh-users/zsh-autosuggestions /home/$username/.zsh/zsh-autosuggestions &> /dev/null 
